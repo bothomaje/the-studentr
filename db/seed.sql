472a4b1a42243678708018637ec5f515e8b82395
bothomaje <bothomaje@gmail.com>
2025-09-02 00:57:54 +0200

    Update schema.sql and seed.sql

---
+++ db/seed.sql
@@ -0,0 +1,12 @@
+USE the_studentr;
+START TRANSACTION;
+
+INSERT INTO USERS (user_id, username, email, password_hash, first_name, surname)
+VALUES ('000000000000000000000000000000000001', 'testuser1', 'testuser1@mail.com', '', 'Test', 'One',)
+ON DUPLICATE KEY UPDATE email=VALUES(email);
+
+INSERT INTO MODULES (module_id, user_id, module_code, module_name, year_mark_weight, exam_weight, min_assignments)
+VALUES
+('000000000000000000000000000000000101', '000000000000000000000000000000000001', 'COS2611', 'Programming: Data Structures', 20, 80, 1)
+('000000000000000000000000000000000102', '000000000000000000000000000000000001', 'INF2611', 'Visual Programming II', 30, 70, 1)
+ON DUPLICATE KEY UPDATE module_name=VALUES(module_name);
