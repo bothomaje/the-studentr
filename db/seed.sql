bfa8008b7ad510458d417dad69a360550c4e7b1d
bothomaje <bothomaje@gmail.com>
2025-09-02 01:06:46 +0200

    Update seed.sql

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@ USE the_studentr;
 START TRANSACTION;
 
 INSERT INTO USERS (user_id, username, email, password_hash, first_name, surname)
-VALUES ('000000000000000000000000000000000001', 'testuser1', 'testuser1@mail.com', '', 'Test', 'One',) AS new
+VALUES ('000000000000000000000000000000000001', 'testuser1', 'testuser1@mail.com', '', 'Test', 'One') AS new
 ON DUPLICATE KEY UPDATE email=new.email;
 
 INSERT INTO MODULES (module_id, user_id, module_code, module_name, year_mark_weight, exam_weight, min_assignments)
